c40f6bf63049113ad4d3714011454a5af94544b5
daniel-olson-code <daniel@orphos.cloud>
2024-08-01 21:49:49 -0700

    dummy host update

--- dummy_report_api.py
+++ dummy_report_api.py
@@ -247,6 +247,6 @@ def download_report(report_id: str):
 
 if __name__ == '__main__':
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+    uvicorn.run(app, host='localhost', port=8000)
 
 
